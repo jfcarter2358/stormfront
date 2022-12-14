9adcfc79174ba646cf39053eb1f982a6c7414337
John Carter <jfcarter2358@gmail.com>
2022-12-14 17:25:27 +0000

    FIX: Resolve issue with non-leader application logs 2

--- src/stormfrontd/client/utils.go
+++ src/stormfrontd/client/utils.go
@@ -1,5 +1,12 @@
 package client
 
+import (
+	"encoding/json"
+	"fmt"
+
+	"github.com/jfcarter2358/ceresdb-go/connection"
+)
+
 func contains(s []string, e string) bool {
 	for _, a := range s {
 		if a == e {
@@ -26,4 +33,28 @@ func dedupeNodes(nodes []StormfrontNode) []StormfrontNode {
 	}
 
 	return out
+}
+
+func getHostFromNode(nodeId string) (string, error) {
+	nodeData, err := connection.Query("get record stormfront.node")
+	if err != nil {
+		return "", err
+	}
+
+	var succession []StormfrontNode
+	successionBytes, _ := json.Marshal(nodeData[0]["succession"])
+	json.Unmarshal(successionBytes, &succession)
+
+	if nodeId == Client.Leader.ID {
+		return fmt.Sprintf("%s:%d", Client.Leader.Host, Client.Leader.Port), nil
+	}
+
+	for _, node := range succession {
+		if nodeId == node.ID {
+			return fmt.Sprintf("%s:%d", node.Host, node.Port), nil
+		}
+	}
+
+	return "", fmt.Errorf("no healthy node with id %s exists", nodeId)
+
 }
