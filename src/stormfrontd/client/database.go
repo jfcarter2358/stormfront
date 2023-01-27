07fe60410ff0c9da63990553082f2835e1702a92
John Carter <jfcarter2358@gmail.com>
2023-01-27 19:35:16 +0000

    FIX: Add missing database key

--- src/stormfrontd/client/database.go
+++ src/stormfrontd/client/database.go
@@ -13,7 +13,7 @@ var Collections = map[string]string{
 	"auth":        `{"id":"STRING","access_token":"STRING","refresh_token":"STRING","token_expiration":"STRING","token_issued":"STRING"}`,
 	"api":         `{"token":"STRING"}`,
 	"application": `{"id":"STRING","node":"STRING","name":"STRING","image":"STRING","hostname":"STRING","env":"DICT","ports":"DICT","mounts":"DICT","memory":"INT","cpu":"FLOAT","status":"DICT"}`,
-	"leader":      `{"id":"STRING","succession":"LIST","unhealthy":"LIST","unknown":"LIST"}`,
+	"leader":      `{"id":"STRING","succession":"LIST","unhealthy":"LIST","unknown":"LIST","healthy"}`,
 	"node":        `{"id":"STRING","host":"STRING","port":"INT","system":"DICT","health":"STRING"}`,
 	"client":      `{"id":"STRING","type":"STRING","leader":"DICT","succession":"LIST","unhealthy":"LIST","unknown":"LIST","updated":"STRING","host":"STRING","port":"INT","healthy":"BOOL","applications":"LIST","system":"DICT"}`,
 }
